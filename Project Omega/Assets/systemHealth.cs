3749bb154f555a4044e60c76bdacfea2eedaab1b
Oliver Philbrick <oliveman521@gmail.com>
2020-07-11 18:04:01 -0400

    Systemhealth tweaks

--- Project Omega/Assets/systemHealth.cs
+++ Project Omega/Assets/systemHealth.cs
@@ -8,7 +8,7 @@ public enum ItemType
     Fuel,
     Bullets,
     Rockets,
-    Sensors
+    Sensors,
 }
 
 public class systemHealth : MonoBehaviour
@@ -19,22 +19,14 @@ public class systemHealth : MonoBehaviour
     {
         if (other.gameObject.tag == "Cargo" && other.gameObject.GetComponent<ItemClassification>().itemType == acceptedCargo)
         {
-
             Destroy(other.gameObject);
         }
     }
 
 
-    // Start is called before the first frame update
-    void Start()
+    public void Damage()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 
