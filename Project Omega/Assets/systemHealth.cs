8934fb0d5f74655948f26b31f3530269df90e788
bobby123jack <gphilbri@gmail.com>
2020-07-11 20:34:32 -0400

    Subsystem Health, Wrenches, and Items

--- Project Omega/Assets/systemHealth.cs
+++ Project Omega/Assets/systemHealth.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,23 +12,59 @@ public enum ItemType
     Sensors,
 }
 
+public enum RepairType
+{
+    Resource,
+    Wrench
+}
+
 public class systemHealth : MonoBehaviour
 {
+    public int maxHealth;
+    public int health;
+
     public ItemType acceptedCargo;
+    public RepairType repairType;
+    public ShipSubSystemType systemType;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Cargo" && other.gameObject.GetComponent<ItemClassification>().itemType == acceptedCargo)
+        if (repairType == RepairType.Resource)
         {
-            Destroy(other.gameObject);
+            if (health < maxHealth && other.gameObject.tag == "Cargo" && other.gameObject.GetComponent<ItemClassification>().itemType == acceptedCargo)
+            {
+                health++;
+                CheckRepaired();
+                Destroy(other.gameObject);
+            }
         }
     }
 
+    private void CheckRepaired()
+    {
+        if (health == maxHealth)
+        {
+            GetComponentInParent<ShipSubsystems>().Repair(systemType);
+        }
+    }
 
     public void Damage()
     {
-
+        health = 0;
     }
 
+    public void Repair()
+    {
+        if (repairType == RepairType.Wrench)
+        {
+            health++;
+            CheckRepaired();
+        }
+    }
 
 }
