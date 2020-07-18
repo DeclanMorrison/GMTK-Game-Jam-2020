da87572bac2c78fb223b4c5d10e401cef4b47483
Oliver Philbrick <oliveman521@gmail.com>
2020-07-18 18:31:03 -0400

    More hull Ui improvements. Reactive on hits

--- Project Omega/Assets/Scripts/HullHealthController.cs
+++ Project Omega/Assets/Scripts/HullHealthController.cs
@@ -14,11 +14,13 @@ public class HullHealthController : MonoBehaviour
 
     private float maxHealth;
     public Vector2 breakVelocity;
+    private Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rTrans = GetComponent<RectTransform>();
         slider = GetComponent<Slider>();
         maxHealth = shipSubsystem.health;
@@ -32,40 +34,38 @@ public class HullHealthController : MonoBehaviour
     {
         if (lastFrameHealth > shipSubsystem.health)
         {
-            Debug.Log("Health Lost!");
-
+            //create a chunk of health to drop off the health bar.
             GameObject newLostHealthChunk = Instantiate(lostHealthChunk);
             newLostHealthChunk.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
-            newLostHealthChunk.GetComponent<RectTransform>().anchoredPosition = new Vector3(Scale(0, maxHealth, 13, 13 + 224, shipSubsystem.health), -35, 0);
+            //place new box where at the position of the current health
+            newLostHealthChunk.GetComponent<RectTransform>().anchoredPosition = new Vector3(Scale(0, maxHealth, rTrans.anchoredPosition.x, rTrans.anchoredPosition.x + rTrans.rect.width, shipSubsystem.health), rTrans.anchoredPosition.y, 0);
+            //scale it to be the size of the total lost health
             newLostHealthChunk.GetComponent<RectTransform>().localScale = new Vector3((Mathf.Clamp((lastFrameHealth-shipSubsystem.health)/maxHealth,0,1)),1,1);
+
             newLostHealthChunk.GetComponent<Rigidbody2D>().velocity = breakVelocity;
             Destroy(newLostHealthChunk, 3);
 
-
-
-            //Mathf.Lerp(rTrans.position.x, rTrans.rect.width + rTrans.position.x, Mathf.Lerp(0, maxHealth, shipSubsystem.health));
-            //Left position of chunk : Mathf.Lerp(rTrans.position.x, rTrans.rect.width + rTrans.position.x, Mathf.Lerp(0, maxHealth, shipSubsystem.health));
-            //Instantiate()
-
-            //rTrans.position.x;
-            //rTrans.rect.width;
-            // shipSubsystem.health
-
-
+            //trigger a opacity flash
+            animator.Play("default");
+            animator.Play("UI_opacityPulse");
         }
 
+        //update the slider value
         slider.value = shipSubsystem.health;
 
 
-
+        //go to gameover if health below 0
         if (shipSubsystem.health <= 0)
         {
             SceneManager.LoadScene(2);
         }
 
+        //update the lastfram health. Used for checking for health changes.
         lastFrameHealth = shipSubsystem.health;
      }
 
+
+    //scaling function. Used for scaling one range to another.
     public float Scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
     {
 
